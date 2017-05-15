b30cbae38a986fbbb185e7fd52de1d3e7bb3a44b
Simon Pasquereau <simon.pasquereau@outlook.com>
2017-05-15 11:38:29 +0200

    Création de la classe SourceMariaDB

---
+++ src/model/SourceMariaDB.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package model;
+
+import java.io.FileInputStream;
+import java.net.PasswordAuthentication;
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.mariadb.jdbc.MariaDbDataSource;
+
+/**
+ * @author Simon
+ *
+ */
+public class SourceMariaDB {
+    
+    public static DataSource getSource(PasswordAuthentication login) throws Exception {
+        String user = login.getUserName();
+        String pwd = new String(login.getPassword());
+        Properties properties = new Properties();
+        FileInputStream propertiesFile = new FileInputStream("src/connexion.properties");
+        properties.load(propertiesFile);
+        MariaDbDataSource mdbDataSource = new MariaDbDataSource();
+        mdbDataSource.setPortNumber(Integer.parseInt(properties.getProperty("port")));
+        mdbDataSource.setDatabaseName(properties.getProperty("service"));
+        mdbDataSource.setServerName(properties.getProperty("serveur"));
+        mdbDataSource.setUser(user);
+        mdbDataSource.setPassword(pwd);
+        return mdbDataSource;
+    }
+}
