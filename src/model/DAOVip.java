28d138463717e657a6732f1e2de73ea92cf66a85
Simon Pasquereau <simon.pasquereau@outlook.com>
2017-05-19 10:50:32 +0200

    Implémentation de la méthode getVipPlayedMovies
    getVipPlayedMethod prend en paramètre un vip et retourne une liste des
    visa des films dans lesquels il a joué

--- src/model/DAOVip.java
+++ src/model/DAOVip.java
@@ -4,6 +4,7 @@
 package model;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -19,46 +20,64 @@ import controller.Vip;
  */
 public class DAOVip {
 
-    private final Connection connexion;
+	private final Connection connexion;
 
-    public DAOVip(Connection connexion) {
-        this.connexion = connexion;
-    }
+	public DAOVip(Connection connexion) {
+		this.connexion = connexion;
+	}
 
-    public void getVip(List<Vip> vipList) throws SQLException {
-        String requete = "select * from VIP";
-        Statement stmt = connexion.createStatement();
-        ResultSet rset = stmt.executeQuery(requete);
-         while (rset.next()) {
-        	int idVip = rset.getInt(1);
-            String name = rset.getString(2);
-            String surname1 = rset.getString(3);
-            String surname2 = rset.getString(4);
-            String surname[] = {surname1, surname2};
-            LocalDate birthdate = rset.getDate(5).toLocalDate();
-            String birthplace = rset.getString(6);
-            char roleCodeVIP = rset.getString(7).charAt(0);
-            int idPartner = rset.getInt(8);
-            Vip vip = new Vip(idVip, name, surname, birthdate, birthplace, roleCodeVIP, idPartner);
-            vipList.add(vip);
-        }        
-        rset.close();
-        stmt.close();
-    } // getVip method
-    
-    public List<String> getVipNationalities(Vip vip) throws SQLException {
-    	int idVip = vip.getIdVip();
-    	List<String> vipNationalities = new ArrayList<>();
-        String requete = "SELECT country.country FROM country, VIP, nationality WHERE VIP.idVIP=? and nationality.idVIP=? and country.country=nationality.country";
-        Statement stmt = connexion.createStatement();
-        ResultSet rset = stmt.executeQuery(requete);
-         while (rset.next()) {
-            String nationality = rset.getString(1);
-            vipNationalities.add(nationality);
-        }
-        rset.close();
-        stmt.close();
-        return vipNationalities;
-    } // getNationalities method
-    
+	public void getVip(List<Vip> vipList) throws SQLException {
+		String requete = "select * from VIP";
+		Statement stmt = connexion.createStatement();
+		ResultSet rset = stmt.executeQuery(requete);
+		while (rset.next()) {
+			int idVip = rset.getInt(1);
+			String name = rset.getString(2);
+			String surname1 = rset.getString(3);
+			String surname2 = rset.getString(4);
+			String surname[] = { surname1, surname2 };
+			LocalDate birthdate = rset.getDate(5).toLocalDate();
+			String birthplace = rset.getString(6);
+			char roleCodeVIP = rset.getString(7).charAt(0);
+			int idPartner = rset.getInt(8);
+			Vip vip = new Vip(idVip, name, surname, birthdate, birthplace, roleCodeVIP, idPartner);
+			vipList.add(vip);
+		}
+		rset.close();
+		stmt.close();
+	} // getVip method
+
+	public List<String> getVipNationalities(Vip vip) throws SQLException {
+		int idVip = vip.getIdVip();
+		List<String> vipNationalities = new ArrayList<>();
+		String requete = "SELECT country.country FROM country, VIP, nationality WHERE VIP.idVIP=? and nationality.idVIP=? and country.country=nationality.country";
+		PreparedStatement pstmt = connexion.prepareStatement(requete);
+		pstmt.setInt(1, idVip);
+		pstmt.setInt(2, idVip);
+		ResultSet rset = pstmt.executeQuery(requete);
+		while (rset.next()) {
+			String nationality = rset.getString(1);
+			vipNationalities.add(nationality);
+		}
+		rset.close();
+		pstmt.close();
+		return vipNationalities;
+	} // getVipNationalities method
+
+	public List<Integer> getVipPlayedMovies(Vip vip) throws SQLException {
+		int idVip = vip.getIdVip();
+		List<Integer> vipPlayedMovies = new ArrayList<>();
+		String requete = "SELECT movieVisa FROM playing WHERE idVIP=?";
+		PreparedStatement pstmt = connexion.prepareStatement(requete);
+		pstmt.setInt(1, idVip);
+		ResultSet rset = pstmt.executeQuery(requete);
+		while (rset.next()) {
+			int playedMovie = rset.getInt(1);
+			vipPlayedMovies.add(playedMovie);
+		}
+		rset.close();
+		pstmt.close();
+		return vipPlayedMovies;
+	} // getVipPlayedMovies method
+
 } // DAOVip class
